8ac9c546e15400a8323310824a8770f796275ce7
Yuvraj Patil <yuvraj.patil.it@gmail.com>
2017-11-24 14:05:56 +0530

    Returned JSON response on visit of users uri

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  var user = {
+    name: 'Yuvraj Patil'
+  };
+  res.send(user);
 });
 
 module.exports = router;
